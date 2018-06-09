627288604a313c21c8323a474ad8661dd59f26d4
orionjeong <djflqjfl0209@gmail.com>
2018-06-09 17:37:16 +0900

    productlist category 및 정렬, filter 기능을 위한 controller 추가

---
+++ src/main/java/kr/ac/jejunu/exchange/Controller/MainController.java
@@ -0,0 +1,8 @@
+package kr.ac.jejunu.exchange.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+    //Todo 일단 어쩔 수 없이 템플릿 엔진 사용하는 부분 여기서 표현!! (ex detailPage)
+}
--- src/main/java/kr/ac/jejunu/exchange/WebConfig.java
+++ src/main/java/kr/ac/jejunu/exchange/WebConfig.java
@@ -6,6 +6,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer{
 
 }
