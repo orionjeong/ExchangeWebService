a9cd93aeda07e984f3f07bc81411fa6bd3de863e
orionjeong <djflqjfl0209@gmail.com>
2018-06-09 14:06:44 +0900

    comment Controller aadd

---
+++ src/main/java/kr/ac/jejunu/exchange/Controller/CommnetController.java
@@ -0,0 +1,45 @@
+package kr.ac.jejunu.exchange.Controller;
+
+import kr.ac.jejunu.exchange.Model.Comment;
+import kr.ac.jejunu.exchange.Repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exchange")
+@RequiredArgsConstructor
+public class CommnetController {
+    @Autowired
+    CommentRepository commentRepository;
+
+    @GetMapping("/{id}")
+    public Comment get(@PathVariable Integer id){
+        return commentRepository.findById(id).get();
+    }
+
+    @GetMapping("/list")
+    public List<Comment> list(){
+        return commentRepository.findAll();
+    }
+
+    @PostMapping
+    public Comment create(@RequestBody Comment comment){
+        return commentRepository.save(comment);
+    }
+
+    @PutMapping
+    public void update(@RequestBody Comment comment){
+        commentRepository.save(comment);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id ){
+        commentRepository.delete(commentRepository.findById(id).get());
+    }
+
+    
+
+}
--- src/test/java/kr/ac/jejunu/exchange/CommentTest.java
+++ src/test/java/kr/ac/jejunu/exchange/CommentTest.java
@@ -1,4 +1,74 @@
 package kr.ac.jejunu.exchange;
 
+import kr.ac.jejunu.exchange.Model.Comment;
+import org.hamcrest.collection.IsEmptyCollection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CommentTest {
+    public static final String PATH="/api/comment";
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @Test
+    public void get(){
+        Integer commentId = 1;
+        String userId = "user2";
+        Integer productId = 1;
+        String contents = "첫 댓글입니다";
+
+        Comment comment = restTemplate.getForObject(PATH+"/"+commentId,Comment.class);
+
+        assertThat(comment.getCommentId(), is(commentId));
+        assertThat(comment.getUserId(), is(userId));
+        assertThat(comment.getProdcutId(), is(productId));
+        assertThat(comment.getContents(), is(comment));
+    }
+
+    @Test
+    public void list(){
+        List<Comment> comments= restTemplate.getForObject(PATH+"/list", List.class);
+        assertThat(comments, not(IsEmptyCollection.empty()));
+    }
+
+    @Test
+    public void create(){
+        String userId = "user2";
+        Integer productId = 1;
+        String contents = "댓글입니다";
+        Comment createComment = createComment(userId, productId, contents);
+        validate(userId, contents, productId, createComment);
+    }
+
+    private Comment createComment(String userId, Integer productId, String contents) {
+        Comment comment = new Comment();
+        comment.setUserId(userId);
+        comment.setProdcutId(productId);
+        comment.setContents(contents);
+        return restTemplate.postForObject(PATH,comment, Comment.class);
+    }
+
+    private void validate(String userId, String contents, Integer productId, Comment createComment) {
+        Comment resultComment = restTemplate.getForObject(PATH+"/"+createComment.getCommentId(), Comment.class);
+        assertThat(resultComment.getContents(), is(contents));
+        assertThat(resultComment.getUserId(), is(userId));
+        assertThat(resultComment.getProdcutId(), is(productId));
+    }
+
+
 }
