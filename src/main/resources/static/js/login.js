0721332108d318ce0b5ca90a1d69f00df80d318f
orionjeong <djflqjfl0209@gmail.com>
2018-06-13 20:24:36 +0900

    아이디저장 구현

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -7,8 +7,7 @@ function login() {
         password: $('#password').val(),
     };
     var method = 'POST';
-    if(user.id)
-        method = 'PUT';
+
     requestData(method, user);
     return false;
 }
@@ -18,11 +17,35 @@ function requestData(method, data) {
         method: method,
         contentType: "application/json",
         data: JSON.stringify(data)
-    }).done(function (state) {
-        sessionStorage.setItem("token",token);
-        window.location.href = "/index";
+    }).done(function (response) {
+        sessionStorage.setItem("token",response.token);
+        sessionStorage.setItem("user",response.username);
+        window.location.href = "/view/login";
     });
 }
 $(document).ready(function () {
+
     $('#btn_login').on("click", login);
+    //아이디 저장 구현
+   var checkboxState = localStorage.getItem("check");
+   var sessionUser = sessionStorage.getItem("user");
+    if(checkboxState!=null&&checkboxState=="true"){
+        $("#checkbox").attr("checked", true);
+        localStorage.setItem("check","true");
+        if(sessionUser!=null){
+            console.log(sessionUser);
+            $("#username").val(sessionUser);
+        }
+    }else{
+        $("#checkbox").attr("checked", false);
+        localStorage.setItem("check","false");
+    }
+
+    $("#checkbox").change(function(){ // 체크박스에 변화가 있다면,
+        if($("#checkbox").is(":checked")){ // ID 저장하기 체크했을 때,
+            localStorage.setItem("check","true");
+        }else{ // ID 저장하기 체크 해제 시,
+            localStorage.setItem("check","false");
+        }
+    });
 });
