3aecd7a44f01c47da68357f9859d0079cdb46d70
orionjeong <djflqjfl0209@gmail.com>
2018-06-14 05:59:33 +0900

    listByUsername mapping product and thumbup

--- src/main/java/kr/ac/jejunu/exchange/Repository/ThumbupRepository.java
+++ src/main/java/kr/ac/jejunu/exchange/Repository/ThumbupRepository.java
@@ -3,5 +3,8 @@ package kr.ac.jejunu.exchange.Repository;
 import kr.ac.jejunu.exchange.Model.Thumbup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ThumbupRepository extends JpaRepository<Thumbup, Integer> {
+    List<Thumbup> findAllByUsername(String username);
 }
